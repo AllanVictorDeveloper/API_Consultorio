9a50880474f5fe67fb55dbe411768925ab2ece6f
Allan Santos <developer.allansantos@outlook.com>
2023-01-14 15:12:47 -0300

    Refatorando o codigo

--- CL.Data/Configuration/ClienteConfiguration.cs
+++ CL.Data/Configuration/ClienteConfiguration.cs
@@ -8,7 +8,7 @@ namespace CL.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Cliente> builder)
         {
-            builder.HasKey(x => x.Id);
+            
             builder.Property(x => x.Nome).IsRequired().HasMaxLength(255);
             builder.Property(x => x.Documento).IsRequired().HasMaxLength(50);
             builder.Property(x => x.Telefone).IsRequired().HasMaxLength(12);
